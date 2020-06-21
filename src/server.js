a90706f1a817aa5284ed0b6add2024823fb38fb4
Mostafa Salama <mostafalearning2016@gmail.com>
2020-06-21 14:53:18 +0200

    create the initial app server

--- src/server.js
+++ src/server.js
@@ -1,19 +1,35 @@
+const http = require('http');
+const path = require('path');
+// to use environment variables for .env files
+const dotenv = require('dotenv');
+// Require Express to run server and routes
+const express = require('express');
+// to enable cors
+const cors = require('cors');
+
+// initialize the app
+const app = express();
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
-
-// Start up an instance of app
-
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// body parser is deprecated since express version 4.16
+// we use express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static(path.join(__dirname,'website')));
 
 // Setup Server
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+app.set('port', port);
+server.listen(port, () => {
+	console.log(`server is running at port ${port}`);
+});
