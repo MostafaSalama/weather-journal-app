c802fadb7c72648cad0e7681094898967e3d65c2
Mostafa Salama <mostafalearning2016@gmail.com>
2020-06-21 22:38:08 +0200

    add function to get data from weather map api

--- src/website/app.js
+++ src/website/app.js
@@ -3,5 +3,29 @@
 const apiKey = '76f1aeec7d3a59e992ee01cc42c88559';
 const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+getWeatherData(80014)
+
+function getCurrentDate() {
+    const d = new Date() ;
+    return d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear()
+}
+
+async function getWeatherData(zipCode,userResponse) {
+    // the url to request
+    let url = baseURL + zipCode + `&appid=${apiKey}` ;
+   try{
+       const response = await fetch(url) ;
+       if (response.ok){
+           const data = await response.json() ;
+           return {
+               date : getCurrentDate(),
+               temperature:data.main.temp,
+               userResponse
+           }
+       }
+   }
+   catch (e) {
+       console.log(e) ;
+   }
+}
+
